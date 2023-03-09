89295ceffa65a17d6b5479e987111a06ca126c09
khanhcp <khanhpdhe153664@fpt.edu.vn>
2023-03-09 17:44:32 +0700

    Add forgot.chhtml

---
+++ Project_PRN221/Pages/Views/Home/ForgotPass.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project_PRN221.Pages.Views.Home
+{
+    public class ForgotPassModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
