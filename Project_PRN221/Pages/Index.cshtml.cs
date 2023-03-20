d39dc531d67f9eb8d16d98f3a2b5854755991f27
khanhcp <khanhpdhe153664@fpt.edu.vn>
2023-03-20 12:00:08 +0700

    make router, page detail, home page

--- Project_PRN221/Pages/Index.cshtml.cs
+++ Project_PRN221/Pages/Index.cshtml.cs
@@ -1,20 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Project_PRN221.Model;
 
 namespace Project_PRN221.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        //private readonly ILogger<IndexModel> _logger;
+        private readonly PRN221_SP23Context dbContext;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public List<Post> listPostCN { get; set; } = default!;
+
+        public List<Post> listPostBDVN { get; set; } = default!;
+
+        public List<Post> listPostPremierLeague { get; set; } = default!;
+
+        public List<Post> listPostLaLiga { get; set; } = default!;
+
+        public List<Post> listPostSeriA { get; set; } = default!;
+
+        public List<Image> listImage { get; set; } = default!;
+
+
+        public IndexModel(PRN221_SP23Context context)
         {
-            _logger = logger;
+            this.dbContext = context;
         }
 
         public void OnGet()
         {
 
+            listPostCN = dbContext.Posts.Where(x => x.IdCategory == 5 || x.IdCategory == 4).Take(3).ToList();
+            listPostBDVN = dbContext.Posts.Where(x => x.IdCategory > 0 && x.IdCategory < 5).Take(3).ToList();
+            listPostPremierLeague = dbContext.Posts.Where(x => x.IdCategory == 6).Take(3).ToList();
+            listPostLaLiga = dbContext.Posts.Where(x => x.IdCategory == 7).Take(3).ToList();
+            listPostSeriA = dbContext.Posts.Where(x => x.IdCategory == 8).Take(3).ToList();
+            listImage = dbContext.Images.ToList();
         }
     }
 }
--- Project_PRN221/Pages/Views/Home/DetailNews.cshtml.cs
+++ Project_PRN221/Pages/Views/Home/DetailNews.cshtml.cs
@@ -1,12 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using Project_PRN221.Model;
 
 namespace Project_PRN221.Pages.Views.Home
 {
     public class DetailNewsModel : PageModel
     {
-        public void OnGet()
+        private readonly PRN221_SP23Context dbContext;
+
+        public Post post { get; set; }
+
+        public List<Authur> listAuthur { get; set; }
+
+        public List<Image> listImage { get; set; }
+
+        public DetailNewsModel(PRN221_SP23Context dbContext)
         {
+            this.dbContext = dbContext;
+        }
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            listAuthur = dbContext.Authurs.ToList();
+            listImage = dbContext.Images.ToList();
+
+            if (id == null)
+            {
+                return Content("Không tìm thay id");
+            }
+            post = await dbContext.Posts.FirstOrDefaultAsync(x => x.IdPost == id);
+            if (post == null)
+            {
+                return Content("Không tìm thay account");
+            }
+            return Page();
         }
     }
 }
